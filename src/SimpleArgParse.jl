9cbebb150bd34d69558db466189887a413015701
Ben Elkin <8xe8n-cm@online.de>
2024-03-22 22:17:45 +0100

    add PromptedParser methods

--- src/SimpleArgParse.jl
+++ src/SimpleArgParse.jl
@@ -360,5 +360,8 @@ set_value!(p::PromptedParser, arg, value) = set_value!(p.parser, arg, value)
 add_argument!(p::PromptedParser, arg_short, arg_long; kwargs...) = add_argument!(p.parser, arg_short, arg_long; kwargs...)
 parse_args!(p::PromptedParser, cli_args) = parse_args!(p.parser; cli_args)
 add_example!(p::PromptedParser, example) = add_example!(p.parser, example) 
+help(p::PromptedParser; color=p.color) = help(p.parser; color)
+get_value(p::PromptedParser, arg) = get_value(p.parser, arg)
+
 
 end # module SimpleArgParse
